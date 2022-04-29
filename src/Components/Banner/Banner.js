3046141f6a8c0488980d2245c940ad729a784c72
Imran Hossen <freelancerimran81@gmail.com>
2022-04-29 15:49:37 +0600

    banner section complete

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,72 +1,30 @@
 import React from 'react';
-import { Carousel, Col, Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import './Banner.css'
 import img1 from '../images/home-shoe-3.png'
-import img2 from '../images/home-shoe-2.png'
-import img3 from '../images/home-shoe-1.png'
 
 const Banner = () => {
     return (
         <div className='carousel-section'>
-            <Carousel>
-                <Carousel.Item>
                     <div>
                         <Row>
-                            <Col>
-                                <div className='text-start'>
-                                    <p>Nike Yellow Shoes</p>
+                            <Col lg='6' className='d-flex justify-content-center align-items-center'>
+                                <div className='text-start '>
+                                    <div className='banner-text ms-5 ps-5 mt-5'>
+                                    <span>Nike Yellow Shoes</span>
                                     <h2>Nike Metcon Shoes</h2>
                                     <p>Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Repellat Maiores Et Eos Eaque Veritatis Aut Laboriosam Earum Dolorem Iste Atque.</p>
-                                    <button className='btn btn-primary'>See Details</button>
+                                    <button className='details-btn'>See Details</button>
+                                    </div>
                                 </div>
                             </Col>
-                            <Col>
-                                <div>
+                            <Col  lg='6'>
+                                <div className='banner-img'>
                                     <img src={img1} alt="" />
                                 </div>
                             </Col>
                         </Row>
                     </div>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <div>
-                        <Row>
-                            <Col>
-                                <div>
-                                    <p>Nike Yellow Shoes</p>
-                                    <h2>Nike Metcon Shoes</h2>
-                                    <p>Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Repellat Maiores Et Eos Eaque Veritatis Aut Laboriosam Earum Dolorem Iste Atque.</p>
-                                    <button className='btn btn-primary'>See Details</button>
-                                </div>
-                            </Col>
-                            <Col>
-                                <div>
-                                    <img src={img2} alt="" />
-                                </div>
-                            </Col>
-                        </Row>
-                    </div>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <div>
-                        <Row>
-                            <Col>
-                                <div>
-                                    <p>Nike Yellow Shoes</p>
-                                    <h2>Nike Metcon Shoes</h2>
-                                    <p>Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Repellat Maiores Et Eos Eaque Veritatis Aut Laboriosam Earum Dolorem Iste Atque.</p>
-                                    <button className='btn btn-primary'>See Details</button>
-                                </div>
-                            </Col>
-                            <Col>
-                                <div>
-                                    <img src={img3} alt="" />
-                                </div>
-                            </Col>
-                        </Row>
-                    </div>
-                </Carousel.Item>
-            </Carousel>
         </div>
     );
 };
