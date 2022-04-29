3f3310140847f3ff4de6e7f4763bae52c69fd640
Imran Hossen <freelancerimran81@gmail.com>
2022-04-29 17:44:32 +0600

    inventories item update

--- src/Components/InventoryItems/InventoryItems.js
+++ src/Components/InventoryItems/InventoryItems.js
@@ -8,11 +8,11 @@ const InventoryItems = () => {
         <div className='mt-5 py-5'>
             <Container className='inventory'>
                 <h2><span>Inventory</span> Products</h2>
-                <div>
+                <div className='mt-5'>
                     <div className='inventories'>
-                        <div className='inventory-items'>
+                        <div className='inventory-items p-4'>
                             <Row>
-                                <Col lg='4' md='4'>
+                                <Col lg='4' md='4' className='d-flex justify-content-center align-items-center'>
                                     <div>
                                         <img src={img1} alt="" />
                                     </div>
@@ -24,13 +24,14 @@ const InventoryItems = () => {
                                         <p><span>Price:</span> $199</p>
                                         <p><span>Quantity:</span> 56pcs</p>
                                         <p><span>Supplier Name:</span> Imran Hossen</p>
+                                        <button className='inventory-btn'>Manage Product</button>
                                     </div>
-                                </Col>bb 
+                                </Col>
                             </Row>
                         </div>
-                        <div className='inventory-items'>
+                        <div className='inventory-items p-4'>
                             <Row>
-                                <Col lg='4' md='4'>
+                                <Col lg='4' md='4' className='d-flex justify-content-center align-items-center'>
                                     <div>
                                         <img src={img1} alt="" />
                                     </div>
@@ -42,6 +43,83 @@ const InventoryItems = () => {
                                         <p><span>Price:</span> $199</p>
                                         <p><span>Quantity:</span> 56pcs</p>
                                         <p><span>Supplier Name:</span> Imran Hossen</p>
+                                        <button className='inventory-btn'>Manage Product</button>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
+                        <div className='inventory-items p-4'>
+                            <Row>
+                                <Col lg='4' md='4' className='d-flex justify-content-center align-items-center'>
+                                    <div>
+                                        <img src={img1} alt="" />
+                                    </div>
+                                </Col>
+                                <Col lg='8' md='8'>
+                                    <div className='text-start'>
+                                        <h3>Product Name</h3>
+                                        <p>Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Repellat Maiores Et Eos Eaque Veritatis Aut Laboriosam Earum Dolorem Iste Atque.</p>
+                                        <p><span>Price:</span> $199</p>
+                                        <p><span>Quantity:</span> 56pcs</p>
+                                        <p><span>Supplier Name:</span> Imran Hossen</p>
+                                        <button className='inventory-btn'>Manage Product</button>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
+                        <div className='inventory-items p-4'>
+                            <Row>
+                                <Col lg='4' md='4' className='d-flex justify-content-center align-items-center'>
+                                    <div>
+                                        <img src={img1} alt="" />
+                                    </div>
+                                </Col>
+                                <Col lg='8' md='8'>
+                                    <div className='text-start'>
+                                        <h3>Product Name</h3>
+                                        <p>Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Repellat Maiores Et Eos Eaque Veritatis Aut Laboriosam Earum Dolorem Iste Atque.</p>
+                                        <p><span>Price:</span> $199</p>
+                                        <p><span>Quantity:</span> 56pcs</p>
+                                        <p><span>Supplier Name:</span> Imran Hossen</p>
+                                        <button className='inventory-btn'>Manage Product</button>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
+                        <div className='inventory-items p-4'>
+                            <Row>
+                                <Col lg='4' md='4' className='d-flex justify-content-center align-items-center'>
+                                    <div>
+                                        <img src={img1} alt="" />
+                                    </div>
+                                </Col>
+                                <Col lg='8' md='8'>
+                                    <div className='text-start'>
+                                        <h3>Product Name</h3>
+                                        <p>Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Repellat Maiores Et Eos Eaque Veritatis Aut Laboriosam Earum Dolorem Iste Atque.</p>
+                                        <p><span>Price:</span> $199</p>
+                                        <p><span>Quantity:</span> 56pcs</p>
+                                        <p><span>Supplier Name:</span> Imran Hossen</p>
+                                        <button className='inventory-btn'>Manage Product</button>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
+                        <div className='inventory-items p-4'>
+                            <Row>
+                                <Col lg='4' md='4' className='d-flex justify-content-center align-items-center'>
+                                    <div>
+                                        <img src={img1} alt="" />
+                                    </div>
+                                </Col>
+                                <Col lg='8' md='8'>
+                                    <div className='text-start'>
+                                        <h3>Product Name</h3>
+                                        <p>Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Repellat Maiores Et Eos Eaque Veritatis Aut Laboriosam Earum Dolorem Iste Atque.</p>
+                                        <p><span>Price:</span> $199</p>
+                                        <p><span>Quantity:</span> 56pcs</p>
+                                        <p><span>Supplier Name:</span> Imran Hossen</p>
+                                        <button className='inventory-btn'>Manage Product</button>
                                     </div>
                                 </Col>
                             </Row>
