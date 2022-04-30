fc8e54f0f8165e97093f8810f2bfb5472df399b4
Imran Hossen <freelancerimran81@gmail.com>
2022-04-30 19:48:02 +0600

    social login done

--- src/Components/Authentication/SocialLogin/SocialLogin.js
+++ src/Components/Authentication/SocialLogin/SocialLogin.js
@@ -2,8 +2,32 @@ import React from 'react';
 import { Button, Col, Row } from 'react-bootstrap';
 import googleLogo from '../../images/logo/Google.png'
 import githubLogo from '../../images/logo/GitHub.png'
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Loading from '../../Loading/Loading';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    // const [signInWithGithub, user1, loading1, error1] = useSignInWithGithub(auth);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || "/";
+
+
+    let errorElement;
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error?.message}</p>
+    }
+    if (loading) {
+        return <Loading />
+    }
+    if (user) {
+        navigate(from, { replace: true })
+    }
+
+
+
     return (
         <div className='pb-4'>
             <div className='d-flex align-items-center mb-3'>
@@ -11,10 +35,12 @@ const SocialLogin = () => {
                 <p className='mt-2'>or</p>
                 <div style={{ height: '1px' }} className='bg-dark w-50 me-5 ms-3'></div>
             </div>
+            {errorElement}
             <div>
                 <div className='mb-3'>
                     <Button
-                        style={{ fontSize: '20px'}} className='btn btn-light px-4 py-2 w-75 login-btn'>
+                        onClick={() => signInWithGoogle()}
+                        style={{ fontSize: '20px' }} className='btn btn-light px-4 py-2 w-75 login-btn'>
                         <Row>
                             <Col lg='4' md='3' sm='6' className='text-end '>
                                 <img src={googleLogo} alt="" />
