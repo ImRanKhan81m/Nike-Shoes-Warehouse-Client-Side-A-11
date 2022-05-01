24a5296c1eb7ab8862d0d4714c344b7f84f237ad
Imran Hossen <freelancerimran81@gmail.com>
2022-05-01 22:49:18 +0600

    RequireAuth added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import { Route, Routes } from 'react-router-dom';
-import AddInventory from './AddInventory/AddInventory';
 import './App.css';
+import AddInventory from './Components/AddInventory/AddInventory';
 import Login from './Components/Authentication/Login/Login';
 import Register from './Components/Authentication/Register/Register';
 import Blog from './Components/Blog/Blog';
@@ -9,22 +9,27 @@ import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import ManageInventoriesProduct from './Components/ManageInventories/ManageInventoriesProduct';
 import NotFound from './Components/NotFound/NotFound';
-import UpdateProduct from './UpdateProduct/UpdateProduct';
+import RequireAuth from './Components/RequireAuth/RequireAuth';
+import UpdateProduct from './Components/UpdateProduct/UpdateProduct';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/home' element={<Home/>} />
-        <Route path='/blog' element={<Blog/>} />
-        <Route path='/manage' element={<ManageInventoriesProduct/>} />
-        <Route path='/addInventory' element={<AddInventory/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/register' element={<Register/>} />
-        <Route path='/shoes/:manageId' element={<UpdateProduct/>} />
-        <Route path='*' element={<NotFound/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/manage' element={<ManageInventoriesProduct />} />
+        <Route path='/addInventory' element={<AddInventory />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/shoes/:manageId' element={
+          <RequireAuth>
+            <UpdateProduct />
+          </RequireAuth>
+        } />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
   );
