ea3acda8bf493818b6fd025f47585e769bdcb0b6
Imran Hossen <freelancerimran81@gmail.com>
2022-05-01 15:16:04 +0600

    navigate button done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Collection from '../Collection/Collection';
-import InventoryItems from '../InventoryItems/InventoryItems';
 import InventoryProducts from '../InventoryItems/InventoryProducts';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
-            {/* <InventoryItems/> */}
             <InventoryProducts/>
             <Collection/>
         </div>
--- src/Components/InventoryItems/InventoryProducts.js
+++ src/Components/InventoryItems/InventoryProducts.js
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import InventoryItems from './InventoryItems';
 
 const InventoryProducts = () => {
     const [products, setProducts] = useState([]);
+    const navigate = useNavigate()
   
     useEffect(()=>{
         fetch('http://localhost:5000/shoes')
         .then(res => res.json())
         .then(data=> setProducts(data.slice(0,6)))
-    },[])
+    },[]);
+
+    const handleManage= ()=>{
+        navigate('/manage')
+    }
 
     return (
         <div>
@@ -25,7 +31,7 @@ const InventoryProducts = () => {
                         }
                     </div>
 
-                    <button className='manage-btn mt-5'>Manage Inventories</button>
+                    <button onClick={handleManage} className='manage-btn mt-5'>Manage Inventories</button>
                 </div>
             </div>
         </div>
